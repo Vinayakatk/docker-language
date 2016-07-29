c4968a4ef1dd21411b2bd0f1f480a4e38bb94fd7
vinayakatk <vinayaka113@gmail.com>
2016-07-29 21:12:12 +0200

    dsf
    Change-Id: I89d014d503dfbf2c03a04fcfc398e34cba97f123

--- org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
+++ org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
@@ -10,7 +10,7 @@ public class Activator implements BundleActivator {
 	static BundleContext getContext() {
 
 
-		System.out.println("first");
+		System.out.println("first fdfd ");
 
 		return context;
 		
