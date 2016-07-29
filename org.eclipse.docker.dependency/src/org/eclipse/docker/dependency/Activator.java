abd06e5888bdb72e4e8b42571dc960725edf927a
vinayakatk <vinayaka113@gmail.com>
2016-07-29 16:23:36 +0200

    i am walking on branch 2
    Change-Id: I3a6e9f01792e00e370adb85282e6a70ce55f5ca9

--- org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
+++ org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
@@ -9,7 +9,7 @@ public class Activator implements BundleActivator {
 
 	static BundleContext getContext() {
 
-		System.out.println("i am the fifth  commit from master3");
+		System.out.println("i am walking on branch 2");
 		return context;
 		
 	}
