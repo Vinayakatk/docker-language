ad13a674344caa4c5b87c2d0231a6790a76f770b
vinayakatk <vinayaka113@gmail.com>
2016-07-29 11:09:57 +0200

    master2
    Change-Id: Idcfb3eaef0bc79dc7686a9e5af4237da2ff84eb3

--- org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
+++ org.eclipse.docker.dependency/src/org/eclipse/docker/dependency/Activator.java
@@ -8,7 +8,7 @@ public class Activator implements BundleActivator {
 	private static BundleContext context;
 
 	static BundleContext getContext() {
-		
+		System.out.println("changes");
 		return context;
 		
 	}
